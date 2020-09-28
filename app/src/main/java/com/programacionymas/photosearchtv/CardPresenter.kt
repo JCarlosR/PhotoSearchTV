b856fd20fb4426608151af62db0d047318eacbdd
Juan Ramos <juancagb.17@gmail.com>
2020-09-27 22:52:57 -0500

    Change CardPresenter

--- app/src/main/java/com/programacionymas/photosearchtv/CardPresenter.kt
+++ app/src/main/java/com/programacionymas/photosearchtv/CardPresenter.kt
@@ -6,6 +6,7 @@ import androidx.leanback.widget.Presenter
 import androidx.core.content.ContextCompat
 import android.util.Log
 import android.view.ViewGroup
+import androidx.leanback.widget.BaseCardView
 
 import com.bumptech.glide.Glide
 import com.programacionymas.model.Photo
@@ -17,6 +18,7 @@ import kotlin.properties.Delegates
  */
 class CardPresenter : Presenter() {
     private var mDefaultCardImage: Drawable? = null
+
     private var sSelectedBackgroundColor: Int by Delegates.notNull()
     private var sDefaultBackgroundColor: Int by Delegates.notNull()
 
@@ -25,6 +27,7 @@ class CardPresenter : Presenter() {
 
         sDefaultBackgroundColor = ContextCompat.getColor(parent.context, R.color.default_background)
         sSelectedBackgroundColor = ContextCompat.getColor(parent.context, R.color.selected_background)
+
         mDefaultCardImage = ContextCompat.getDrawable(parent.context, R.drawable.movie)
 
         val cardView = object : ImageCardView(parent.context) {
@@ -37,15 +40,16 @@ class CardPresenter : Presenter() {
         cardView.isFocusable = true
         cardView.isFocusableInTouchMode = true
         updateCardBackgroundColor(cardView, false)
-        return Presenter.ViewHolder(cardView)
+        return ViewHolder(cardView)
     }
 
-    override fun onBindViewHolder(viewHolder: Presenter.ViewHolder, item: Any) {
+    override fun onBindViewHolder(viewHolder: ViewHolder, item: Any) {
         val movie = item as Photo
         val cardView = viewHolder.view as ImageCardView
 
         Log.d(TAG, "onBindViewHolder")
         if (movie.cardImageUrl != null) {
+            cardView.cardType = BaseCardView.CARD_TYPE_INFO_OVER
             cardView.titleText = movie.title
             cardView.contentText = movie.author
             cardView.setMainImageDimensions(CARD_WIDTH, CARD_HEIGHT)
@@ -58,19 +62,25 @@ class CardPresenter : Presenter() {
         }
     }
 
-    override fun onUnbindViewHolder(viewHolder: Presenter.ViewHolder) {
+    override fun onUnbindViewHolder(viewHolder: ViewHolder) {
         Log.d(TAG, "onUnbindViewHolder")
+
         val cardView = viewHolder.view as ImageCardView
         // Remove references to images so that the garbage collector can free up memory
+
         cardView.badgeImage = null
         cardView.mainImage = null
     }
 
     private fun updateCardBackgroundColor(view: ImageCardView, selected: Boolean) {
-        val color = if (selected) sSelectedBackgroundColor else sDefaultBackgroundColor
-        // Both background colors should be set because the view's background is temporarily visible
+        // Both background colors should be set because the background is temporarily visible
         // during animations.
-        view.setBackgroundColor(color)
+
+        val color = if (selected)
+            sSelectedBackgroundColor
+        else
+            sDefaultBackgroundColor
+
         view.setInfoAreaBackgroundColor(color)
     }
 
