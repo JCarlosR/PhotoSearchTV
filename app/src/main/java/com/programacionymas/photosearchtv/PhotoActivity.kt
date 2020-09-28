6c0cb40ec59557b6d74cd3f4090cca6cdfcac6ce
Juan Ramos <juancagb.17@gmail.com>
2020-09-27 22:03:49 -0500

    Show one image at full size

--- app/src/main/java/com/programacionymas/photosearchtv/PhotoActivity.kt
+++ app/src/main/java/com/programacionymas/photosearchtv/PhotoActivity.kt
@@ -1,20 +1,29 @@
 package com.programacionymas.photosearchtv
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
+import com.bumptech.glide.Glide
+import com.programacionymas.model.Photo
+import kotlinx.android.synthetic.main.activity_photo.*
 
-/** Loads [PhotoFragment]. */
 class PhotoActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        // setContentView(R.layout.activity_details)
+        setContentView(R.layout.activity_photo)
 
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                    .replace(android.R.id.content, PhotoFragment())
-                    .commit()
-        }
+        val (_, title, author, imageUrl) =
+            intent?.getSerializableExtra(PhotoActivity.MOVIE) as Photo
+
+        // mSelectedMovie = activity?.intent?.getSerializableExtra(DetailsActivity.MOVIE) as Movie
+
+        Toast.makeText(this, imageUrl, Toast.LENGTH_SHORT).show()
+
+        Glide.with(this)
+            .load(imageUrl)
+            .centerCrop()
+            .into(imageView)
     }
 
     companion object {
