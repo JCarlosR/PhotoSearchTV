af0b259e69301e00cea1e0fbad7da550b02b84f9
Juan Ramos <juancagb.17@gmail.com>
2020-09-28 22:31:51 -0500

    Listen key events in PhotoActivity

--- app/src/main/java/com/programacionymas/photosearchtv/activity/PhotoActivity.kt
+++ app/src/main/java/com/programacionymas/photosearchtv/activity/PhotoActivity.kt
@@ -1,6 +1,8 @@
 package com.programacionymas.photosearchtv.activity
 
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.Toast
 import androidx.fragment.app.FragmentActivity
 import com.bumptech.glide.Glide
 import com.programacionymas.model.Photo
@@ -19,6 +21,24 @@ class PhotoActivity : FragmentActivity() {
             .load(photo.getUrlLarge())
             .centerCrop()
             .into(imageView)
+    }
+
+
+    override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+        // we will execute actions for the DOWN event
+        if (event.action == KeyEvent.ACTION_UP) {
+            return true
+        }
+
+        if (event.keyCode == KeyEvent.KEYCODE_DPAD_RIGHT) {
+            Toast.makeText(baseContext, "Right", Toast.LENGTH_SHORT).show()
+        } else if (event.keyCode == KeyEvent.KEYCODE_DPAD_LEFT) {
+            Toast.makeText(baseContext, "Left", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(baseContext, "Keycode = " + event.keyCode, Toast.LENGTH_SHORT).show()
+        }
+
+        return true
     }
 
     companion object {
